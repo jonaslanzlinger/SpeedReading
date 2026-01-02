483056b5e45d2c532c8de27cc7bf6baebfd573df
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2026-01-02 17:38:24 +0100

    Initial Commit

---
+++ SpeedReading/SpeedReadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedReadingApp.swift
+//  SpeedReading
+//
+//  Created by Jonas Länzlinger on 02.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpeedReadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
